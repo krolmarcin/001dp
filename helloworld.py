7543e1fcb1d098af0fbbc7893c4eb984a85f4f89
Marcin <szeryfmk@gmail.com>
2022-10-23 11:39:07 +0200

    first commit

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+def print_hw():
+    hw = 'Hello World'
+    print(hw)
+
+
+def second_function():
+    print('sec_function')
+
+
+print_hw()
+second_function()
+
+if second_function() == print('sec_function'):
+    print('nothing')
